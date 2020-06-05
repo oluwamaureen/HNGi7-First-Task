b341c43e3693a377c803eb7d36d97f69e0a6a8bd
oluwamaureen <66250841+oluwamaureen@users.noreply.github.com>
2020-06-05 16:12:07 +0100

    Create 01726-maureen.py

---
+++ scripts/01726-maureen.py
@@ -0,0 +1,7 @@
+full_name = "Maureen Onabajo"
+HNGi_ID = "HNG-01726"
+language_used = "Python"
+msg = f'Hello world, this is {full_name} with HNGi7 ID {HNGi_ID} using {language_used} for stage 2 task'
+email = "isetire@gmail.com"
+msg = f'Hello World, this is {full_name} with HNGi7 ID {HNGi_ID} using {language_used} for stage 2 task and {email}'
+print(msg)
